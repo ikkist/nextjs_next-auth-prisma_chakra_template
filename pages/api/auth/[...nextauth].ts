ff930bb6df1e55c1e90b1e8abe3b192f02606655
ikki <kazuki.seto@gmail.com>
2021-01-16 21:34:28 +0900

    define schema of prisma and do migration

---
+++ pages/api/auth/[...nextauth].ts
@@ -0,0 +1,18 @@
+import { NextApiHandler } from "next";
+import NextAuth from "next-auth";
+import Providers from "next-auth/providers";
+import Adapters from "next-auth/adapters";
+import prisma from "../../../lib/prisma";
+
+const authHandler: NextApiHandler = (req, res) => NextAuth(req, res, options);
+export default authHandler;
+
+const options = {
+  providers: [
+    Providers.Google({
+      clientId: process.env.GOOGLE_CLIENT_ID,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+    }),
+  ],
+  adapter: Adapters.Prisma.Adapter({ prisma }),
+};

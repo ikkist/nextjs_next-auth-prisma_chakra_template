0fd3c5200ad55d470fe7b8d7b8bf642b1c986405
ikki <kazuki.seto@gmail.com>
2021-01-23 15:34:49 +0900

    support react-query package

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,11 +1,15 @@
 import { AppProps } from "next/app";
 import { Provider } from "next-auth/client";
+import { QueryClient, QueryClientProvider } from "react-query";
 
+const queryClient = new QueryClient();
 function App({ Component, pageProps }: AppProps) {
   return (
-    <Provider session={pageProps.session}>
-      <Component {...pageProps} />
-    </Provider>
+    <QueryClientProvider client={queryClient}>
+      <Provider session={pageProps.session}>
+        <Component {...pageProps} />
+      </Provider>
+    </QueryClientProvider>
   );
 }
 
